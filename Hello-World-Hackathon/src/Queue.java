a53049af4e339baa051586a0a11fe399b43c3985
psuu7 <112496611+psuu7@users.noreply.github.com>
2022-09-17 20:52:36 -0400

    Update Queue.java
    Added Court class; Made queue model more realistic and close to finished product.

--- Hello-World-Hackathon/src/Queue.java
+++ Hello-World-Hackathon/src/Queue.java
@@ -2,36 +2,48 @@ import java.util.*;
 
 public class Queue {
 
-    public static List<List<Integer>>  findGroup(List<List<Integer>> q, int groupRequest) {
+    public static List<List<String>>  findGroup(List<List<String>> q, int groupRequest) {
         // q is formatted as such:
         // [userID, number of members in group]
 
         //Stores group members currently in group
-        List<List<Integer>> currGroup = new ArrayList<>();
+        List<List<String>> currGroup = new ArrayList<>();
 
         //Tracks if a member that can be removed from the queue was found
         boolean found = true;
 
-        while (found == true) {
+        //Tracks members to be removed from q
+        List<List<String>> toRemove = new ArrayList<>();
+
+        while (found) {
             //Every for loop must find a new user, else break
             found = false;
 
-            for (List<Integer> i : q) {
+            //Checks if queue is empty
+            if (q.size() == 0)
+                return currGroup;
+
+            for (List<String> i : q) {
                 // If a value is found, flip found
-                if (i.get(1) < groupRequest) {
-                    groupRequest -= i.get(1);
+                if (Integer.parseInt(i.get(1)) < groupRequest) {
+                    groupRequest -= Integer.parseInt(i.get(1));
                     currGroup.add(i);
+                    toRemove.add(i);
                     found = true;
 
-                } else if (i.get(1) == groupRequest) {
+                //If value is equal to the remaining amount, break
+                } else if (Integer.parseInt(i.get(1)) == groupRequest) {
                     groupRequest = 0;
                     currGroup.add(i);
 
                     //Ensures while break, even though it is true
                     found = false;
                     break;
+
                 }
             }
+            //Removes all items from q after the end of a for loop
+            q.removeAll(toRemove);
         }
 
         return currGroup;
@@ -39,28 +51,67 @@ public class Queue {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List<List<Integer>> q = new ArrayList<>();
 
-        System.out.println("Enter Group Request Size");
-        int groupRequest = scan.nextInt();
-        scan.nextLine();
+        //Initializes Queue
+        List<List<String>> q = new ArrayList<>();
 
+        //In the future this should be replaced by user input from server files
         System.out.println("Enter number of members in queue");
         int queueSize = scan.nextInt();
         scan.nextLine();
 
         for (int i = 0; i < queueSize; i++) {
-            System.out.println((i+1) + ". Enter member ID and group size");
-            int userID = scan.nextInt();
-            int groupSize = scan.nextInt();
+            System.out.println((i + 1) + ". Enter member ID and group size");
+            String[] temp = scan.nextLine().split("\\s+");
+            String userID = temp[0];
+            String groupSize = temp[1];
             q.add(Arrays.asList(userID, groupSize));
         }
 
-        List<List<Integer>> ans =  findGroup(q, groupRequest);
+        //In the future this should call the list of all courts
+        List<Court> courts = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            List<List<String>> temp_l = new ArrayList<>();
+            courts.add(new Court(i+1, 10, 0, temp_l ));
 
-        System.out.println("Remove from queue:");
-        for (List<Integer> i : ans)
-            System.out.println(i);
+        }
+        //Sort the courts by player availability -- Reverse Bubble sort
+        for (int i = 0; i < courts.size(); i++) {
+            for (int j = i; j < courts.size(); j++) {
+                int c1 = courts.get(i).totalCapacity - courts.get(i).currCapacity;
+                int c2 = courts.get(j).totalCapacity - courts.get(j).currCapacity;
 
+                if (c1 < c2) {
+                    Court temp = courts.get(i);
+                    courts.set(i, courts.get(j));
+                    courts.set(j, temp);
+                }
+            }
+        }
+
+        //If player availability > 0, request more players from queue
+        for (Court i : courts) {
+            int availability = i.totalCapacity - i.currCapacity;
+
+            // If availability = 0, all subsequent availabilities will be 0.
+            // If q size is 0, no more users are in queue
+            if (availability == 0 || q.size() == 0) {
+                break;
+            } else {
+                List<List<String>> playerChange;
+
+                //Calls findGroup function to
+                playerChange = findGroup(q, availability);
+                q.removeAll(playerChange);
+                i.playerList.addAll(0, playerChange);
+
+            }
+        }
+
+        for (Court i : courts) {
+            System.out.println(i.courtID);
+            System.out.println(i.playerList);
+            System.out.printf("\n");
+        }
     }
 }
